572449f5a2b7b6575c3d2d353956da1671a595a4
sadee <srrohan333@gmail.com>
2021-05-29 22:27:59 +0600

    conflict change

--- src/Components/Nav/NavLink.js
+++ src/Components/Nav/NavLink.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
-import Login from '../Auth/Login';
 import CardDetail from '../Card-detail/CardDetail';
 import Home from '../Home/Home';
 
@@ -9,7 +8,6 @@ const NavLink = () => {
   return (
     <div>
       <Route exact path="/"><Home></Home></Route>
-      <Route exact path="/login"><Login></Login></Route>
       <Route exact path="/card-details"><CardDetail></CardDetail></Route>
     </div>
   );
