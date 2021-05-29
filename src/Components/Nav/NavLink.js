dff01786561166ef8d08678049d0336fcb5b0087
sadee <srrohan333@gmail.com>
2021-05-29 22:32:17 +0600

    conflict issue

--- src/Components/Nav/NavLink.js
+++ src/Components/Nav/NavLink.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
+import Login from '../Auth/Login';
 import CardDetail from '../Card-detail/CardDetail';
 import Home from '../Home/Home';
 
@@ -8,6 +9,7 @@ const NavLink = () => {
   return (
     <div>
       <Route exact path="/"><Home></Home></Route>
+      <Route exact path="/login"><Login></Login></Route>
       <Route exact path="/card-details"><CardDetail></CardDetail></Route>
     </div>
   );
