6126b5660abf595349a084bbcb38fec6d52c6d09
mdehsanul <mdehsanulhaqueshanto04@gmail.com>
2021-05-30 00:01:29 +0600

    home page done

--- src/Components/Nav/NavLink.js
+++ src/Components/Nav/NavLink.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import Login from "../Authentication/Login";
-import Signup from "../Authentication/Signup";
-import CardDetail from "../Card-detail/CardDetail";
+import Login from "../Auth/Login";
 import Home from "../Home/Home";
 
 const NavLink = () => {
@@ -13,12 +11,6 @@ const NavLink = () => {
       </Route>
       <Route exact path="/login">
         <Login></Login>
-      </Route>
-      <Route exact path="/signup">
-        <Signup></Signup>
-      </Route>
-      <Route exact path="/card-details">
-        <CardDetail></CardDetail>
       </Route>
     </div>
   );
